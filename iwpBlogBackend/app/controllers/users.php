44efd0ade83aa19d8c0a7bbae49018ff2731ad9a
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2021-05-13 18:33:19 +0530

    added login and logout

--- iwpBlogBackend/app/controllers/users.php
+++ iwpBlogBackend/app/controllers/users.php
@@ -10,7 +10,26 @@
   $email = '';
   $password = '';
   $passwordConf = '';
+  $table = 'users';
   $errors = array();
+
+  function loginUser($user){
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+    $_SESSION['admin'] = $user['admin'];
+    $_SESSION['message'] = 'You are now logged in';
+    $_SESSION['type'] = 'success';
+
+    if ($_SESSION['admin']) {
+      header('location:' . BASE_URL . '/admin/dashboard.php');
+    } else {
+      header('location:' . BASE_URL . '/index.php');
+    }
+    
+
+    header('location: ' . BASE_URL . '/index.php');
+    exit();
+  }
 
   if(isset($_POST['register-btn'])){
 
@@ -20,25 +39,11 @@
           unset($_POST['register-btn'], $_POST['passwordConf']);
           $_POST['admin'] = 0;
           $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-          $user_id = create('user',$_POST);
-          $user = selectOne('user', ['id' => $user_id]);
+          $user_id = create($table,$_POST);
+          $user = selectOne($table, ['id' => $user_id]);
           //log user in
-          $_SESSION['id'] = $user['id'];
-          $_SESSION['username'] = $user['username'];
-          $_SESSION['admin'] = $user['admin'];
-          $_SESSION['message'] = 'You are now logged in';
-          $_SESSION['type'] = 'success';
-
-          if ($_SESSION['admin']) {
-            header('location:' . BASE_URL . '/admin/dashboard.php');
-          } else {
-            header('location:' . BASE_URL . '/index.php');
-          }
-          
-
-          header('location: ' . BASE_URL . '/index.php');
-          exit();
-
+          loginUser($user);
+         
         } else{
           $username = $_POST['username'];
           $email = $_POST['email'];
@@ -47,5 +52,20 @@
         }
 
         
+  }
+
+  if(isset($_POST['login-btn'])){
+    $errors = validateLogin($_POST);
+    if(count($errors)===0){
+      $user = selectOne($table, ['username' => $_POST['username']]);
+      if($user && password_verify($_POST['password'], $user['password'])){
+        //log user in
+        loginUser($user);
+      }else{
+        array_push($errors, 'Wrong Credentials');
+      }
+    }
+    $username = $_POST['username'];
+    $password = $_POST['password'];
   }
 ?>
--- iwpBlogBackend/app/helpers/validateUser.php
+++ iwpBlogBackend/app/helpers/validateUser.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 function validateUser($user){
     
     $errors = array();
@@ -13,12 +14,31 @@ function validateUser($user){
       if(empty($user['password'])){
         array_push($errors, 'Password is required.');
       }
+      $uppercase = preg_match('@[A-Z]@', $user['password']);
+      $lowercase = preg_match('@[a-z]@', $user['password']);
+      $number    = preg_match('@[0-9]@', $user['password']);
+
+      if(!$uppercase || !$lowercase || !$number || strlen($user['password']) < 8) {
+        array_push($errors, 'Password must contain 8 characters, one uppercase, one lowercase, one number.');
+      }
       if(($user['password'] !== $user['passwordConf'])){
         array_push($errors, 'Passwords do not match');
       }
       $existingUser = selectOne('user', ['email' => $user['email']]);
       if(isset($existingUser)){
         array_push($errors, 'Email already exists');      
+      }
+    return $errors;
+}
+
+function validateLogin($user){
+    
+    $errors = array();
+    if(empty($user['username'])){
+        array_push($errors, 'Username is required.');
+      }
+      if(empty($user['password'])){
+        array_push($errors, 'Password is required.');
       }
     return $errors;
 }
